526d6f50f0b5687e8665067df94c492e7554aa0c
Kushal Basak <kushalbasak14581@gmail.com>
2023-03-27 01:34:14 +0530

    added

--- frontend/src/Components/Carausal.js
+++ frontend/src/Components/Carausal.js
@@ -1,61 +1,35 @@
-import React from "react"
-import Carousel from "react-multi-carousel"
-import "react-multi-carousel/lib/styles.css";
-const Vcarousel=()=>{
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Smallvideo from "./Smallvideo";
+const Vcarousel = () => {
+  const [videos, setVideos] = useState([]);
+  // const [lessVideos, setLessVideos] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
 
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
+  useEffect(() => {
+    const GetData = async () => {
+      const url = `http://localhost:8080/api/videos/all`;
+
+      const res = await axios.get(url, {
+        headers: {
+          token: "Bearer " + currentUser.accessToken,
         },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    return(
-        <>
-         <div className="cra">
-        <Carousel responsive={responsive}>
-            <div>
-                <img className="Cimg" src="https://images.indianexpress.com/2020/03/RRR-Motion-Poster-759.jpg" alt="img1"/>
-                    
-            </div>
-            <div>
-            <img className="Cimg" src="https://www.themoviedb.org/t/p/original/tdQzRSk4PXX6hzjLcQWHafYtZTI.jpg" alt="img2"/>
-            
-                    
-            </div>
-            <div>
-            <img className="Cimg" src="https://feeds.abplive.com/onecms/images/uploaded-images/2021/07/10/273e787ba809e725948334340c658e68_original.jpg?impolicy=abp_cdn&imwidth=720" alt="img3"/>
-           
-            
-                    
-            </div>
-            <div>
-            <img className="Cimg" src="https://media-cache.cinematerial.com/p/500x/zqbh2w6s/prey-movie-poster.jpg?v=1658509864" alt="img4"/>
-            
-            </div>
-            <div>
-            <img className="Cimg" src="https://www.filmibeat.com/ph-big/2022/07/ghost_165760618910.jpg" alt="img5"/>
-            
-            </div>
-            <div>
-            <img className="Cimg" src="https://filmfare.wwmindia.com/content/2022/jun/jawaan41654408101.jpg" alt="img6"/>
-           
-            </div>
-        </Carousel>
-        </div>
-        </>
-    )
-}
+      });
+      setVideos(res.data.slice(0, 5));
+    };
+
+    GetData();
+  }, [currentUser]);
+
+  console.log(videos);
+
+  return (
+    <div className="view-less">
+      {videos.map((video) => (
+        <Smallvideo video={video} />
+      ))}
+    </div>
+  );
+};
 export default Vcarousel;
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,48 +1,59 @@
 import React from "react";
-import Nav from "./NavBar"
+import Nav from "./NavBar";
 import axios from "axios";
-import {useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import FetchVideo from "./VideoFetch";
-const HomePage=()=> {
-  
-  const [videos, setVideos] = useState([])
+const HomePage = () => {
+  const [videos, setVideos] = useState([]);
+  const [userName, setUserName] = useState("");
   const { currentUser } = useSelector((state) => state.user);
-  const GetData=()=>{
-      // const key = '541e4e6113f4a245be41d653a35850a6'
-      const url = `http://localhost:8080/api/videos/get-random`
-      const getPic = () => {
-          axios.get(url,{
-            headers: {
-              token: "Bearer " + currentUser.accessToken,
-            },
-          })
-              .then((res) => setVideos(Object.values(res.data)))
-              .catch((err) => { console.log(err) })
-              .finally(() => { })
-          console.log(videos);
-          // console.log(currentUser.accessToken)
-      }
-      getPic()
-  }
-  
-  useEffect(()=>{
-   
-    GetData();
 
-  },[])
+  useEffect(() => {
+    const GetData = async () => {
+      const url = `http://localhost:8080/api/videos/get-random`;
+
+      const res = await axios.get(url, {
+        headers: {
+          token: "Bearer " + currentUser.accessToken,
+        },
+      });
+      setVideos(res.data);
+
+      if (res.data) {
+        const getUser = await axios.get(
+          `http://localhost:8080/api/users/get-User/${res.data[0].publisherId}`
+        );
+        setUserName(getUser.data.username);
+      }
+    };
+
+    GetData();
+  }, [currentUser]);
+
+  console.log(videos, userName);
   return (
     <>
-   <Nav/>
-    <div>
-      <div className="Image">
-        {/* {videos.map((data,index)=>{
-          return( */}
-          <video  controls src={videos.videoUrl} type="video/mp4"/>
-        {/* )})} */}
-      
-      </div>
-      {/* <div id="filmName">
+      <Nav />
+      <div>
+        <div className="Image">
+          <video src={videos[0]?.videoUrl} type="video/mp4" />
+          <h2 className="video-title">{videos[0]?.title}</h2>
+          <div className="video-data">
+            <p>14 Jan, 2023</p>
+            <p>14 Mins</p>
+            <p>200 views</p>
+          </div>
+          <div className="publisher">
+            <img
+              className="publisher-img"
+              src="https://img.freepik.com/premium-psd/psd-3d-male-cartoon-character-avatar-isolated-3d-rendering_460336-1517.jpg?w=740"
+              alt=""
+            />
+            <p>{userName}</p>
+          </div>
+        </div>
+        {/* <div id="filmName">
         <span className="film">Midway</span> <br />
         <span className="subtitle">Base On Real Events</span>
       </div>
@@ -58,15 +69,13 @@ const HomePage=()=> {
         />
         <span>user name</span>
       </div> */}
-    </div>
-    
-    <FetchVideo/>
+      </div>
+
+      <FetchVideo />
     </>
   );
-}
+};
 
 export default HomePage;
-
-
 
 // "https://raw.githubusercontent.com/Newton-School/audio-video-HTML-cypress-boilerplate/main/public/ghoul.mp4"
