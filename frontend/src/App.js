e657a7938126b13ebe152cb9ada9ceef9282c096
Kushal Basak <kushalbasak14581@gmail.com>
2023-03-26 18:25:32 +0530

    changed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-
-import './App.css';
-import HomePage from './Components/Home';
-import LoadingPage1 from './Components/Home';
-import FetchVideo from './Components/VideoFetch';
-import Nav from './Components/NavBar';
-import SigninSignup from './Components/signupLogin';
-import VideoUpload from './Components/Vupload';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/Home";
+// import LoadingPage1 from "./Components/Home";
+// import FetchVideo from "./Components/VideoFetch";
+// import Nav from "./Components/NavBar";
+import Signup from "./Components/signup";
+import Login from "./Components/login";
+import VideoUpload from "./Components/Vupload";
 
 function App() {
   return (
-    <div className="App">
-     {/* <SigninSignup/> */}
-     {/* <VideoUpload/> */}
-    {/* <Nav/> */}
-    
-    {/* <FetchVideo/> */}
-    <HomePage/>
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/upload" element={<VideoUpload />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
