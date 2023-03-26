c90a8405c79dce301fd69d4042958daf6a781ee8
Digvijay <digbjssn@gmail.com>
2023-03-26 22:16:57 +0530

    added

--- frontend/src/Components/VideoFetch.js
+++ frontend/src/Components/VideoFetch.js
@@ -1,76 +1,63 @@
 import React, { useEffect ,useState} from "react";
+import { useSelector } from "react-redux";
 import axios from 'axios';
+import Vcarousel from "./Carausal";
 const FetchVideo=()=> {
-    const [photos, setPhotos] = useState([])
-    const [inp,setinp]=useState('river')
+    const [viewAll,setviewAll]=useState(true)
+    const [videos, setVideos] = useState([])
+    const { currentUser } = useSelector((state) => state.user);
     const GetData=()=>{
-        const key = '541e4e6113f4a245be41d653a35850a6'
-        const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${inp}&per_page=24&format=json&nojsoncallback=1`
+        // const key = '541e4e6113f4a245be41d653a35850a6'
+        const url = `http://localhost:8080/api/videos/all`
         const getPic = () => {
-            axios.get(url)
-                .then((res) => setPhotos(Object.values(res.data.photos.photo)))
+            axios.get(url,{
+              headers: {
+                token: "Bearer " + currentUser.accessToken,
+              },
+            })
+                .then((res) => setVideos(Object.values(res.data)))
                 .catch((err) => { console.log(err) })
                 .finally(() => { })
-            // console.log(input,cate);
-            
+            // console.log(videos);
+            // console.log(currentUser.accessToken)
         }
         getPic()
     }
-    GetData();
-   
+    
     useEffect(()=>{
-      setinp("")
-    },[])
+     
+      GetData();
+    })
    
     
    
     return (
-      <div className="Fetch-container">
-       <div className="util">
-        <p>Recent</p>
-        <p>View All</p>
+      <>
+      <div className="util">
+        {<button>Recent</button>}
+        {viewAll?<button onClick={()=>{setviewAll(false)}}>View Less</button>:<button onClick={()=>{setviewAll(true)}}>View All</button>}
        </div>
-       <div className="Vid">
-        {photos.map((task,index)=>{
-            return(
-                <div key={index} className='F-img'>
-                    <img className='F-images' key={task.id} src={`https://live.staticflickr.com/${task.server}/${task.id}_${task.secret}_m.jpg`} alt={task.title} />
-                </div>
-            )
-        })}
+       <div>
+        {viewAll?(
+          <div className="Vid">
+          {videos.map((task,index)=>{
+              return(
+                  <div key={index} className='F-img'>
+                      <video controls className='F-images' key={task.id} src={task.videoUrl} alt={task.title} />
+                  </div>
+              )
+          })}
+         </div>
+        ):<Vcarousel/>}
        </div>
-      </div>
+      </>
+     
     );
   }
   export default FetchVideo;
 
 
 
-  /** <div className="morethings">
-          <a href="/" className="recent">
-            Recent
-          </a>
-          <a href="/" className="viewall">
-            View All
-          </a>
-        </div>
   
-        <div class="row">
-          <div class="column">
-            <div class="image-container">
-              <img
-                src="https://th.bing.com/th/id/OIP.KWX4RI7xFtrDcJbXw_AgdQHaE7?pid=ImgDet&rs=1"
-                alt="moviePoster"
-              />
-              <div class="text-overlay">
-                <span className="film1">Midway</span> <br />
-                <span className="subtitle1">Base On Real Events</span>
-                <div id="details1">
-                  <span className="date1">12 Jan 2023</span>
-                  <span className="length1"> 14 Mins</span>
-                  <span className="views1"> 200 Views</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */
+    
+  
